ab922a564784ed73a4d87ffbfa7a846d24c422c7
Maria <s9657680851@gmail.com>
2025-12-26 14:49:27 +0300

    add record

--- programming/laba3/src/main/java/Memory.java
+++ programming/laba3/src/main/java/Memory.java
@@ -1,11 +1,4 @@
-public class Memory {
-    private boolean isForgot;
-    private String topic;
-
-    public Memory(boolean isForgot, String topic) {
-        this.isForgot = false;
-        this.topic = topic;
-    }
+public record Memory(boolean isForgot, String topic) {
 
     public String getDescription() {
         String flashback;
