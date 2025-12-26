7bd0c48d95530c931c650cc4ec984c5e95ff4a68
Maria <s9657680851@gmail.com>
2025-12-26 14:46:42 +0300

    add interface

--- programming/laba3/src/main/java/Door.java
+++ programming/laba3/src/main/java/Door.java
@@ -1,5 +1,5 @@
 import Enum.Time;
-public class Door {
+public class Door implements Openable{
     private boolean isOpen;
     private Entrance entrance;
 
@@ -8,6 +8,19 @@ public class Door {
         this.entrance = entrance;
     }
 
+    @Override
+    public void open() {
+        this.isOpen = true;
+        System.out.println("Дверь открылась");
+    }
+    @Override
+    public void close() {
+        close(1);
+    }
+    @Override
+    public boolean isOpen() {
+        return isOpen;
+    }
     public void close(int force) {
         this.isOpen = false;
         if (force >= 5) {
